d2e05b5f054fd122697ac7eb2f0b2de489d68db2
Joshua Meyers <joshua.r.meyers@gmail.com>
2017-06-27 16:53:20 -0500

    add comments

--- chaining.js
+++ chaining.js
@@ -4,8 +4,20 @@
     // Multiply each remaining number by 1.5 and then subtract 1.
     // Then output (either in the DOM or the console) the sum of all the resulting numbers.
 
-
-console.log("reverse sorty", [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8].sort(function(a, b) {return b - a;}).filter(function(numbers){return numbers < 19;}).map(function(multi) {return multi * 1.5 - 1;}).reduce(function(c, d) {return c + d;}));
+//instead of var = [array], you can declare the array before the function
+//use "b - a" to sort in descending order (a - b will be low to high)
+console.log("reverse sorty", [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8].sort(function(a, b) {
+	return b - a;
+	//.filter to remove array items
+}).filter(function(numbers){
+	return numbers < 19;
+	//.map to apply math to all items in array
+}).map(function(multi) {
+	return multi * 1.5 - 1;
+	//.reduce to "reduce" array into one item (adds array items. used c + d similar to how the sort method works)
+}).reduce(function(c, d) {
+	return c + d;
+}));
 
 
 
